67a25cb4b58369df9bdf8ae16d8475b579b82503
Jacob Cox <jcox@deseretdigital.com>
2018-10-05 14:30:04 -0600

    Routing and nativebase oh my.

--- App.js
+++ App.js
@@ -3,28 +3,49 @@ import { Permissions } from 'expo'
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import { NativeRouter, Switch, Route, Link } from 'react-router-native'
+import { Container, Header, Content, Card, CardItem, Icon, Right } from 'native-base';
 import ImagePicker from './components/ImagePicker';
 import ReduxExample from './components/ReduxExample';
+import ListButton from './components/ListButton';
+
+
 
 export default class App extends React.Component {
-  async getPermissions() {
-    const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
-    if (status === 'granted') {
-      ImagePicker.launchImageLibraryAsync({mediaTypes: Expo.ImagePicker.ALL})
-    }
-  }
+  // async getPermissions() {
+  //   const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+  //   if (status === 'granted') {
+  //     ImagePicker.launchImageLibraryAsync({mediaTypes: Expo.ImagePicker.ALL})
+  //   }
+  // }
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <ReduxExample />
-        </View>
+        <NativeRouter>
+          <Switch>
+            <Route exact path="/" render={() => 
+              <Container>
+                <Header />
+                <Content>
+                  <ListButton to='classifieds' text='Classifieds' iconLeft='tool' />
+                  <ListButton to='cars' text='Cars' iconLeft='car' />
+                  <ListButton to='homes' text='homes' iconLeft='home' />
+                  <ListButton to='jobs' text='jobs' iconLeft='ios-briefcase' />
+                </Content>
+              </Container>
+            } />
+            <Route path="/classifieds" render={() => <View><Text>Classifieds</Text></View>} />
+            <Route path="/cars" render={() => <Text>Cars</Text>} />
+            <Route path="/homes" render={() => <Text>Homes</Text>} />
+            <Route path="/jobs" render={() => <Text>Jobs</Text>} />
+          </Switch>
+        </NativeRouter>
       </Provider>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/ListButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-native'
+import { Container, Header, Content, Card, CardItem, Icon, Right, Text } from 'native-base';
+
+
+
+export default class ListButton extends React.Component {
+  static propTypes = {
+    to: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    leftIcon: PropTypes.string.isRequired,
+    rightIcon: PropTypes.string
+  }
+  static defaultProps = {
+    rightIcon: "ios-arrow-forward"
+  }
+  render() {
+    return (
+      <Card>
+        <CardItem>
+          <Icon active name={this.props.iconLeft} />
+          <Link to={`/${this.props.to}`}>
+            <Text>{this.props.text}</Text>
+          </Link>
+          <Right>
+            <Icon name="ios-arrow-forward" />
+          </Right>
+        </CardItem>
+      </Card>
+    );
+  }
+}
