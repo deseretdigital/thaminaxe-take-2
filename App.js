06b3356805f978401f96c7b2212edcccab61106b
Abe Fawson <afawson@deseretdigital.com>
2019-03-08 14:30:27 -0700

    fix font issue on android

--- App.js
+++ App.js
@@ -9,6 +9,20 @@ import Home from './components/Home';
 import Cars from './components/Cars';
 
 export default class App extends React.Component {
+
+  state = {
+    isReady: false
+  };
+
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({isReady:true})
+  }
+
   // async getPermissions() {
   //   const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
   //   if (status === 'granted') {
@@ -16,6 +30,9 @@ export default class App extends React.Component {
   //   }
   // }
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
     return (
       <Provider store={store}>
         <Container>
